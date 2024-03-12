15f6a29ef62ac410d909773b750ef01cdf3f990e
tnpham23 <tpham848664@gmail.com>
2024-03-12 16:04:10 -0700

    git commit-m fixed email?

--- send_email.php
+++ send_email.php
@@ -1,50 +1,40 @@
 <?php
-$errors = [];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get POST data
-    $name = isset($_POST['name']) ? strip_tags(trim($_POST['name'])) : '';
-    $email = isset($_POST['email']) ? trim($_POST['email']) : '';
-    $message = isset($_POST['message']) ? strip_tags(trim($_POST['message'])) : '';
 
-    // Validate form fields
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
+require './phpmailer\src\Exception.php';
+require './phpmailer\src\PHPMailer.php';
+require './phpmailer\src\SMTP.php';
 
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
+if (isset($_POST["Send"])) {
+    $mail = new PHPMailer(true);
 
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
+    // Configure the PHPMailer instance
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'pham.thuongn22@gmail.com';
+    $mail->Password = 'rvrpxgaejbzzppao';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 465;
 
-    // If no errors, send email
-    if (empty($errors)) {
-        // Recipient email address (replace with your own)
-        $recipient = "tpham848664@gmail.com";
+    // Set the sender, recipient, subject, and body of the message 
+    $mail->setFrom('pham.thuongn22@gmail.com');
+    $mail->addAddress($_POST["email"]);
+    $mail->isHTML(true);
+    $mail->Subject = 'New email from your website';
+    $mail->Body = $_POST["message"];
 
-        // Additional headers
-        $headers = "From: $name <$email>";
-
-        // Send email
-        if (mail($recipient, $message, $headers)) {
-            echo "Email sent successfully!";
-        } else {
-            echo "Failed to send email. Please try again later.";
-        }
-    } else {
-        // Display errors
-        echo "The form contains the following errors:<br>";
-        foreach ($errors as $error) {
-            echo "- $error<br>";
-        }
-    }
-} else {
-    // Not a POST request, display a 403 forbidden error
-    header("HTTP/1.1 403 Forbidden");
-    echo "You are not allowed to access this page.";
-} ?>
+    // Send the message
+    $mail->send () ;
+    echo
+    "
+    <script>
+    alert('Sent Successfully');
+    document.location.href = 'index.html';
+    </script>
+    ";
+  }
+?>
